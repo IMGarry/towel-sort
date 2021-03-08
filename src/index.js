f670eeb0e1ac1b515f57fd7ec71bdd23bd6f9eda
Igor Marchenko <igor.ms83@gmail.com>
2021-03-08 11:03:47 +0300

    docs: add my solution to index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) return []
+  return matrix.map((it, index) => (index % 2 !== 0) ? it.sort((a,b) => b-a) : it).flat()
 }
